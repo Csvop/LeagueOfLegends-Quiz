f312ead4ace08faf29d8a12b78d25594a92ca96a
Bruno Garcia <brunobreyergarcia@gmail.com>
2021-02-12 05:58:30 -0300

    v1.2

--- src/components/LolQuizlogo/index.js
+++ src/components/LolQuizlogo/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 function QuizLogo({ className }) {
     return (
-      <img width="95%" src="https://i.ibb.co/M2DrKzh/Logo-Lo-Lquiz.png"></img>
+      <img width="80%" src="https://i.ibb.co/M2DrKzh/Logo-Lo-Lquiz.png"></img>
     );
   }
   
